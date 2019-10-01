691be0ef3b9d18a2024e35b932b5a1686f3e93d1
Majid Jaidi <majid.jaidi@gmail.com>
2019-10-01 10:15:40 +0100

    updated callback url in production

--- src/auth/auth0-config.js
+++ src/auth/auth0-config.js
@@ -1,7 +1,7 @@
-import { AUTH0_DOMAIN_ID, AUTH0_CLIENT_ID } from "../constants";
+import { AUTH0_DOMAIN_ID, AUTH0_CLIENT_ID, CALLBACK_URL } from "../constants";
 
 export const AUTH_CONFIG = {
   domain: AUTH0_DOMAIN_ID,
   clientId: AUTH0_CLIENT_ID,
-  callbackUrl: "http://localhost:3000/callback"
+  callbackUrl: CALLBACK_URL
 };
